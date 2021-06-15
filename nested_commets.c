7be4dcafbd6ba4c2ca18e071bb049b6a15ec6578
Naveenbittu <85916508+Naveenbittu@users.noreply.github.com>
2021-06-15 22:54:59 +0530

    Add files via upload

---
+++ nested_commets.c
@@ -0,0 +1,11 @@
+//1.write a programme for nested comments.what are the error&how to reslove it
+#include<stdio.h>
+int main()
+{
+    /*programme for hello world
+   /* printf("hello world\n");*/
+    printf("hi to every one\n")*/
+}
+
+//output: expected expression before '\'token
+//resolve by using remove the inside comment and put the '//' in between programme.
